f47d47ae4c49881c72892ba2f3b6bf93a66c9631
Author Name <email@address.com>
2021-10-09 16:19:38 -0300

    first commit

---
+++ src/test/java/com/estudos/reactivejava/ReactiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.reactivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
