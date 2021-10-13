802749fd6d8c3095644972ef399ffd96db735ee8
joao4018 <joao4018@gmail.com>
2021-10-13 18:40:17 -0300

    estudos programacao reativa

---
+++ src/test/java/com/estudos/reactivejava/FluxTest.java
@@ -0,0 +1,192 @@
+package com.estudos.reactivejava;
+
+import java.time.Duration;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class FluxTest {
+
+    @Test
+    public void fluxSubscriber() {
+        Flux<String> fluxString = Flux.just("Joao", "Carlos", "Aline", "Amor")
+            .log();
+
+        StepVerifier.create(fluxString)
+            .expectNext("Joao", "Carlos", "Aline", "Amor")
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbers() {
+        Flux<Integer> flux = Flux.range(1, 5)
+            .log();
+
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("-----------------------------------");
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5)
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberFromList() {
+        Flux<Integer> flux = Flux.fromIterable(List.of(1, 2, 3, 4, 5))
+            .log();
+
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("-----------------------------------");
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5)
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersError() {
+        Flux<Integer> flux = Flux.range(1, 5)
+            .log()
+            .map(i -> {
+                if (i == 4) {
+                    throw new IndexOutOfBoundsException("index error");
+                }
+                return i;
+            });
+
+        flux.subscribe(i -> log.info("Number {}", i), Throwable::printStackTrace,
+            () -> log.info("DONE!"), subscription -> subscription.request(3));
+
+        log.info("-----------------------------------");
+
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3)
+            .expectError(IndexOutOfBoundsException.class)
+            .verify();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersUglyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+            .log();
+
+        flux.subscribe(new Subscriber<>() {
+            private int count = 0;
+            private Subscription subscription;
+            private final int requestCount = 2;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                subscription.request(requestCount);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                count++;
+                if (count >= requestCount) {
+                    count = 0;
+                    subscription.request(requestCount);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        log.info("-----------------------------------");
+
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersNotSoUglyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+            .log();
+
+        flux.subscribe(new BaseSubscriber<>() {
+            private int count = 0;
+            private final int requestCount = 2;
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(requestCount);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                count++;
+                if (count >= requestCount) {
+                    count = 0;
+                    request(requestCount);
+                }
+            }
+        });
+
+        log.info("-----------------------------------");
+
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberPrettyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+            .log()
+            .limitRate(3);
+
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("-----------------------------------");
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberIntervalOne() throws Exception {
+        Flux<Long> interval = Flux.interval(Duration.ofMillis(100))
+            .take(10)
+            .log();
+
+        interval.subscribe(i -> log.info("Number {}", i));
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void fluxSubscriberIntervalTwo() throws Exception {
+        StepVerifier.withVirtualTime(this::createInterval)
+            .expectSubscription()
+            .expectNoEvent(Duration.ofDays(1))
+            .thenAwait(Duration.ofDays(1))
+            .expectNext(0L)
+            .thenAwait(Duration.ofDays(1))
+            .expectNext(1L)
+            .thenCancel()
+            .verify();
+    }
+
+    private Flux<Long> createInterval() {
+        return Flux.interval(Duration.ofDays(1))
+            .log();
+    }
+
+
+}
